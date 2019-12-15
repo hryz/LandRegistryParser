beed66c73f35e8833fd6929f1409304174b2c5be
Anton Hryzodub <anton.grizodub@gmail.com>
2019-12-15 12:59:45 +0200

    Ported to .Net Core, got rid of UI, made the console generate CSV instead of JSON

--- ConsoleRunner/Program.cs
+++ ConsoleRunner/Program.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 using LandRegistryParser;
-using Newtonsoft.Json;
 using StructureConverter;
 
 namespace ConsoleRunner
@@ -18,14 +19,54 @@ namespace ConsoleRunner
         {
             if (args.Length != 2)
             {
-                Console.WriteLine("Please pass 2 arguments: the source PDF file, and the result JSON file");
+                Console.WriteLine("Please pass 2 arguments: the source PDF file, and the result CSV file");
                 Console.ReadLine();
                 return;
             }
             var model = Converter.Convert(args[0]);
             var result = FuncParser.Parse(model, KeyOffset, ValueOffset, DelimeterOffset).Skip(1); //skip report indo
             var models = ModelConverter.ConvertDictionaryToModels(result);
-            File.WriteAllText(args[1], JsonConvert.SerializeObject(models, Formatting.Indented));
+            File.WriteAllText(args[1], FormatCsv(models));
+        }
+
+        static string FormatCsv(List<Owner> owners)
+        {
+            var sb = new StringBuilder();
+            var props = typeof(Owner).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            //write a header
+            for (int i = 0; i < props.Length; i++)
+            {
+                var prop = props[i];
+                sb.Append("\"").Append(prop.Name).Append("\"");
+                if (i == props.Length - 1)
+                {
+                    sb.AppendLine();
+                }
+                else
+                {
+                    sb.Append(",");
+                }
+            }
+            sb.AppendLine("");
+            foreach (var owner in owners)
+            {
+                for (int i = 0; i < props.Length; i++)
+                {
+                    var prop = props[i];
+                    var value = prop.GetValue(owner);
+                    sb.Append("\"").Append(value).Append("\"");
+                    if (i == props.Length - 1)
+                    {
+                        sb.AppendLine();
+                    }
+                    else
+                    {
+                        sb.Append(",");
+                    }
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }
